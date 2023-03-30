d78ad616676cbd8e7400dc6abadcc926a4e0373b
UsamaShahzad7 <mughalosama618@gmail.com>
2023-03-30 12:59:43 +0500

    Server Changes

--- server.js
+++ server.js
@@ -9,31 +9,35 @@ app.use(cors());
 app.use(express.json());
 app.use("/", router);
 app.listen(5000, () => console.log("Server Running"));
-console.log(process.env.EMAIL_USER);
-console.log(process.env.EMAIL_PASS);
+// console.log(process.env.EMAIL_USER);
+// console.log(process.env.EMAIL_PASS);
 
-const contactEmail = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
-    user: "********@gmail.com",
-    pass: ""
-  },
-});
+// const contactEmail = nodemailer.createTransport({
+//   service: 'gmail',
+//   auth: {
+//     user: "********@gmail.com",
+//     pass: ""
+//   },
+// });
 
-contactEmail.verify((error) => {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log("Ready to Send");
-  }
-});
+// contactEmail.verify((error) => {
+//   if (error) {
+//     console.log(error);
+//   } else {
+//     console.log("Ready to Send");
+//   }
+// });
 
 router.post("/contact", (req, res) => {
   const name = req.body.firstName + req.body.lastName;
   const email = req.body.email;
   const message = req.body.message;
   const phone = req.body.phone;
-  const mail = {
+  console.log({name,email,message,phone});
+  res.send("Message sent");
+
+
+  /*const mail = {
     from: name,
     to: "********@gmail.com",
     subject: "Contact Form Submission - Portfolio",
@@ -48,5 +52,5 @@ router.post("/contact", (req, res) => {
     } else {
       res.json({ code: 200, status: "Message Sent" });
     }
-  });
+  });*/
 });
